eb41d20d0ab2f555dd5476d52f99da63415a17ce
박진오 <jinoh1030@naver.com>
2025-07-21 15:26:28 +0900

    feat: 11003문제 풀이 수정

--- C++pratice/11003.cpp
+++ C++pratice/11003.cpp
@@ -2,26 +2,36 @@
 #include <deque>
 #include <vector>
 using namespace std;
+
 int main() {
-	int n, l;
-	cin >> n >> l;
-	deque<pair<int, int>> dq;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	for (int i = 0; i < n; i++) {
-		int now;
-		cin >> now;
+    int n, m;
+    deque<pair<int, int>> dq;
+    cin >> n >> m;
 
-		while (!dq.empty() && dq.back().first > now) {
-			dq.pop_back();
-		}
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        
+        //덱에선 새롭게 들어오는 수와 큐의 뒤에서 부터 비교하여
+        //항상 오름차순을 유지하고 가장 작은 값이 앞에 오게 유지
+        while (!dq.empty() && dq.back().second >= num) {
+            dq.pop_back();
+        }
 
-		dq.push_back({ now, i });
+        //새롭게 들어온 숫자는 항상 덱 뒤에 삽입
 
-		if (dq.front().second <= i - 1) {
-			dq.pop_front();
-		}
+        dq.push_back({ i, num });
 
-		cout << dq.front().first << " ";
-	}
-	return 0;
+        //범위를 벗어난 최솟값은 큐의 맨앞에 항상 존재하므로 삭제
+        if (dq.front().first <= i - m) {
+            dq.pop_front();
+        }
+
+        cout << dq.front().second<< " ";
+    }
+   
+    return 0;
 }
