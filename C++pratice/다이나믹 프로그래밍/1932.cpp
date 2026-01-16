318263ba6ee8021e5501e3eccaedb5b95933d85c
박진오 <jinoh1030@naver.com>
2026-01-16 19:30:05 +0900

    1932번 문제 주석추가

--- C++pratice/다이나믹 프로그래밍/1932.cpp
+++ C++pratice/다이나믹 프로그래밍/1932.cpp
@@ -2,37 +2,42 @@
 #include <vector>
 using namespace std;
 
-int main(){
+int main() {
 	int n;
 	cin >> n;
 
-	vector<vector<int>> tri(n + 1, vector<int>(n + 1));
+	vector<vector<int>> tri(n + 1, vector<int>(n+1));
 	vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-	for (int i = 1; i <= n ; i++) {
+	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
 			cin >> tri[i][j];
 		}
 	}
+	//dp[i][j] = 꼭대기부터 (i, j) 위치까지 내려왔을 때의 누적 최대 합
+	//dp[i][j] = max(위에서 올 수 있는 값들) + 자기 자신
 
+	//초기 dp값
 	dp[1][1] = tri[1][1];
 
 	for (int i = 2; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
+			//맨 왼쪽(경계, 이전 경로는 정해져 있음)
 			if (j == 1) {
 				dp[i][j] = dp[i - 1][j] + tri[i][j];
 			}
+			//맨 오른쪽(경계, 이전 경로는 정해져 있음)
 			else if (j == i) {
 				dp[i][j] = dp[i - 1][j - 1] + tri[i][j];
 			}
+			// 가운데
 			else {
-				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + tri[i][j];
+				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j])+tri[i][j];
 			}
 		}
-
 	}
 
-
+	//최대값은 마지막 층에서 가장 큰 값
 	int answer = 0;
 	for (int i = 1; i <= n; i++) {
 		answer = max(answer, dp[n][i]);
@@ -41,3 +46,5 @@ int main(){
 	cout << answer;
 	return 0;
 }
+
+
