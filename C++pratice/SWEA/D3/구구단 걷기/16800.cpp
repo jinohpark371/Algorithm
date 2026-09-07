22c9b08add6aa40ed16f762727946b424c9e5780
jinohpark371 <jinoh1030@naver.com>
2026-09-07 16:12:22 +0900

    SWEA D3 16800 구구단걷기 풀이

---
+++ C++pratice/SWEA/D3/구구단 걷기/16800.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    long long n;
+    cin >> n;
+    long long ans = LLONG_MAX;
+    // 약수 짝 중 작거나 같은 쪽 찾기
+    for (long long j = 1; j * j <= n; j++)
+    {
+      if (n % j == 0)
+      {
+        long long r = n / j;
+        ans = min(ans, (j - 1) + (r - 1));
+      }
+    }
+
+    cout << '#' << i + 1 << ' ' << ans << '\n';
+  }
+}
